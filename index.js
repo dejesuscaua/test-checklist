5a6894bc6140cedbf5ed106575a099191db0c758
Dejesuscaua <cauagjesus02@gmail.com>
2025-04-28 10:40:54 -0300

    test

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const fs = require('fs');
 const FormData = require('form-data');
 const formidable = require('formidable');
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
- 
+
 module.exports = async function (context, req) {
   const form = new formidable.IncomingForm({ keepExtensions: true });
- 
+
   await new Promise((resolve, reject) => {
     form.parse(req, async (err, fields, files) => {
       if (err) {
@@ -14,20 +14,20 @@ module.exports = async function (context, req) {
         reject();
         return;
       }
- 
+
       const fileArray = files.arquivo;
       const itemId = fields.itemId;
       const coluna = fields.coluna || "file_mkpn46xc"; // coluna padrão
- 
+
       if (!fileArray || !itemId) {
         context.res = { status: 400, body: { error: "Arquivo ou itemId ausente." } };
         reject();
         return;
       }
- 
+
       const file = Array.isArray(fileArray) ? fileArray[0] : fileArray;
       const filePath = file?.filepath || file?.path;
- 
+
       const formData = new FormData();
       formData.append("query", `
         mutation ($file: File!) {
@@ -36,7 +36,7 @@ module.exports = async function (context, req) {
           }
         }`);
       formData.append("variables[file]", fs.createReadStream(filePath));
- 
+
       try {
         const response = await fetch("https://api.monday.com/v2/file", {
           method: "POST",
@@ -45,7 +45,7 @@ module.exports = async function (context, req) {
           },
           body: formData
         });
- 
+
         const result = await response.json();
         context.res = { status: 200, body: result };
         resolve();
@@ -56,4 +56,4 @@ module.exports = async function (context, req) {
       }
     });
   });
-}
+};
